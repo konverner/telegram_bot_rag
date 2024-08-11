efc5cf7556c95055ef81845181263b4de6e44b58
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2024-08-11 13:58:18 +0200

    Update main.py

--- src/telegram_bot_rag/main.py
+++ src/telegram_bot_rag/main.py
@@ -1,5 +1,5 @@
-from telegram_bot.api.telegram import start_bot
-from telegram_bot.db.database import create_tables
+from telegram_bot_rag.api.telegram import start_bot
+from telegram_bot_rag.db.database import create_tables
 
 if __name__ == "__main__":
     create_tables()
